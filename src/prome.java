0363c54e8fcc45653ec9b52b437d87fe0762fc3b
EMMANUEL <Hi @EmmanuelRB!>
2017-12-01 23:03:37 -0600

    este programa calcula el promedio de 4 notas ingresadas por el usuario

---
+++ src/prome.java
@@ -0,0 +1,9 @@
+public class prome {
+    double n1=9,n2=5,n3=10,n4=9;
+    double promedi;
+
+    public void promedio(){
+        promedi=(n1+n2+n3+n4)/4;
+        System.out.println("el promedio es "+ promedi);
+    }
+}
